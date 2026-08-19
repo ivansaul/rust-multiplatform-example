95e25d81ecbcaf745eddec4028e92f7e29a8019e
ivansaul <saul_2503@hotmail.com>
2026-08-18 20:46:02 -0500

    chore(rust): remove storage module and redb implementation

--- rust/rusty-core/src/storage/key_value_storage.rs
+++
@@ -1,7 +0,0 @@
-use crate::storage::error::StorageError;
-
-pub trait KeyValueStorage {
-    fn get(&self, key: &str) -> Result<Option<Vec<u8>>, StorageError>;
-    fn set(&self, key: &str, value: &[u8]) -> Result<(), StorageError>;
-    fn remove(&self, key: &str) -> Result<(), StorageError>;
-}
