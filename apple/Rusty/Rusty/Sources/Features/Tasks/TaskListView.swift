d5268c5924e41c33634903b9c69bf79563edc050
ivansaul <saul_2503@hotmail.com>
2026-09-09 16:07:29 -0500

    fix(tasks): update add task icon

--- apple/Rusty/Rusty/Sources/Features/Tasks/TaskListView.swift
+++ apple/Rusty/Rusty/Sources/Features/Tasks/TaskListView.swift
@@ -43,7 +43,7 @@ struct TaskListView: View {
                 Button {
                     showSheet.toggle()
                 } label: {
-                    Image(systemName: "plus.circle.dashed")
+                    Image(systemName: "plus.circle")
                 }
             }
         }
